847c23fab3b50e92623b8ab5a0cc526a0e3fca86
baguscs <cahyob59@gmail.com>
2023-06-16 17:41:45 +0700

    update and delete employee

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -78,7 +78,10 @@ class EmployeeController extends Controller
      */
     public function edit(Employee $employee)
     {
-        //
+        $pageTitle = ' Edit Employee';
+        $positions = Position::all();
+
+        return view('app.employee.edit', compact('pageTitle', 'employee', 'positions'));
     }
 
     /**
@@ -86,7 +89,31 @@ class EmployeeController extends Controller
      */
     public function update(Request $request, Employee $employee)
     {
-        //
+        $messages = [
+            'required' => ':Attribute harus diisi.',
+            'email' => 'Isi :attribute dengan format yang benar',
+            'numeric' => 'Isi :attribute dengan angka'
+        ];
+
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'age' => 'required|numeric',
+        ], $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $employee->firstname = $request->firstName;
+        $employee->lastname = $request->lastName;
+        $employee->age = $request->age;
+        $employee->email = $request->email;
+        $employee->position_id = $request->position;
+        $employee->save();
+
+        return redirect()->route('employees.index')->with('success', 'Berhasil Mengupdate Data');
     }
 
     /**
@@ -94,6 +121,7 @@ class EmployeeController extends Controller
      */
     public function destroy(Employee $employee)
     {
-        //
+        $employee->delete();
+        return redirect()->route('employees.index');
     }
 }
