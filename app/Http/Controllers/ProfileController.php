a446f1901357183e9d04a1a9bb89b6b1afa898ff
Fajri <fajrii.fadhil12@gmail.com>
2023-06-17 18:19:49 +0700

    profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\ProfileController;
 
 class ProfileController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('profile', ProfileController::class)->name('profile');
