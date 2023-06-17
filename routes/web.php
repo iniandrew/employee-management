6e84683bd81caae770543320c4c46954880dc7bc
spyreddd <adyanizza5353@gmail.com>
2023-06-17 15:31:12 +0700

    add missing file in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
